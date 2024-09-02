59b10e79c14eb1a14ca35fdafb8f2b42a7c8b53f
MJ Sarmiento <111588224+mojsarmiento@users.noreply.github.com>
2024-09-02 17:17:13 +0800

    first commit

---
+++ crud_flutter_api/database/migrations/2024_09_02_072236_create_students_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstName');
+            $table->string('lastName');
+            $table->string('course');
+            $table->enum('year', ['First Year', 'Second Year', 'Third Year', 'Fourth Year', 'Fifth Year']);
+            $table->boolean('enrolled');
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
